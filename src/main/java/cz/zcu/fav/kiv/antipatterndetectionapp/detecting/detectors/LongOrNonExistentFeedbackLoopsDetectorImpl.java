32e7e2cb3f6db9c643d996bbb6d2519f3d50e3d2
Ondřej Váně <onry.vane@gmail.com>
2021-04-12 14:33:34 +0200

    Basic implementation long or non existatn feedback loops

---
+++ src/main/java/cz/zcu/fav/kiv/antipatterndetectionapp/detecting/detectors/LongOrNonExistentFeedbackLoopsDetectorImpl.java
@@ -0,0 +1,99 @@
+package cz.zcu.fav.kiv.antipatterndetectionapp.detecting.detectors;
+
+import cz.zcu.fav.kiv.antipatterndetectionapp.detecting.DatabaseConnection;
+import cz.zcu.fav.kiv.antipatterndetectionapp.model.AntiPattern;
+import cz.zcu.fav.kiv.antipatterndetectionapp.model.Project;
+import cz.zcu.fav.kiv.antipatterndetectionapp.model.QueryResultItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LongOrNonExistentFeedbackLoopsDetectorImpl extends AntiPatternDetector {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(LongOrNonExistentFeedbackLoopsDetectorImpl.class);
+
+    private final AntiPattern antiPattern = new AntiPattern(6L,
+            "Long Or Non Existent Feedback Loops",
+            "LongOrNonExistentFeedbackLoops",
+            "Long spacings between customer feedback or no feedback. The customer " +
+                    "enters the project and sees the final result. In the end, the customer " +
+                    "may not get what he really wanted. With long intervals of feedback, " +
+                    "some misunderstood functionality can be created and we have to spend " +
+                    "a lot of effort and time to redo it. ");
+
+    private final String SQL_FILE_NAME = "long_or_non_existent_feedback_loops.sql";
+
+    /**
+     * Settings
+     */
+    private final float DIVISION_LONG_OR_NON_FEEDBACK_ITERATIONS = (float) 1/3;
+
+    /* Settings */
+    private final int MINIMUM_NUMBER_OF_WIKI_PAGES = 1;
+    private final int MINIMUM_NUMBER_OF_ACTIVITIES = 1;
+
+    @Override
+    public AntiPattern getAntiPatternModel() {
+        return this.antiPattern;
+    }
+
+    @Override
+    public String getAntiPatternSqlFileName() {
+        return this.SQL_FILE_NAME;
+    }
+
+    /**
+     * Postup detekce:
+     *  A)
+     *      1) najít všechny aktivity, které by mohli indikovat schůzku se zákazníkem
+     *      2) udělat join na iterace
+     *      3) v ideálním případě by měla mít každá iterace jeden nalezený issue
+     *      4) pokud nebude nalezen žádný iisue, tak to znamená že tým nezaznamenává schůzky do úkolů => je nutné ještě prověřit wiki stránky
+     *      5) zkusit nalézt všechny wiki stránky, které by mohly souviset s demem
+     *      6) dle datumu úpravy udělat join na iterace (jedna wiki více schůzek)
+     *      7) v ideálním případě by měla mít opět každá iterace jeden záznam
+     *      8) dle prahových hodnot vyhodnotit výsledky
+     *
+     * @param project            analyzovaný project
+     * @param databaseConnection databázové připojení
+     * @param queries            list sql dotazů
+     * @return výsledek detekce
+     */
+    @Override
+    public QueryResultItem analyze(Project project, DatabaseConnection databaseConnection, List<String> queries) {
+        Long totalNumberIterations = 0L;
+        Map<String, Integer> iterationsResults = new HashMap<>();
+
+        List<List<Map<String, Object>>> resultSets = databaseConnection.executeQueriesWithMultipleResults(project, queries);
+        for (int i = 0; i < resultSets.size(); i++) {
+            List<Map<String, Object>> rs = resultSets.get(i);
+
+            if (i == 0) {
+                totalNumberIterations = (Long) rs.get(0).get("numberOfIterations");
+            }
+
+            if (i == 1) {
+                String iterationName;
+                for (Map<String, Object> map : rs) {
+                    iterationName = (String) map.get("iterationName");
+                    iterationsResults.put(iterationName, 1);
+                }
+            }
+        }
+        int minFeedbackLimit =  totalNumberIterations.intValue() - Math.round(totalNumberIterations * DIVISION_LONG_OR_NON_FEEDBACK_ITERATIONS);
+
+        if ((totalNumberIterations.intValue() != iterationsResults.size())) {
+            if (totalNumberIterations - iterationsResults.size() > minFeedbackLimit) {
+                return new QueryResultItem(this.antiPattern, true , null);
+            } else {
+                return new QueryResultItem(this.antiPattern, false , null);
+
+            }
+        } else {
+            return new QueryResultItem(this.antiPattern, false , null);
+        }
+    }
+}
--- src/main/resources/queries/long_or_non_existent_feedback_loops.sql
+++ src/main/resources/queries/long_or_non_existent_feedback_loops.sql
@@ -14,26 +14,13 @@ Detection: How to choose what is the optimal spacing between feedbacks? In ASWI,
            leaders log time on it (essentially a similar issue as in the anti-Business
            as usual model). Search for an activity named "DEMO", "CUSTOMER", etc.
            Search for some records from the demo in the wiki. Similar to Business as usual.
-
-           POZNÁMKY:
-                     1) naléz aktivity, které mohou odpovídat schůzce se zákazníkem
-                     2) nalezené aktivity zgrupovat podle dueDate
-                     3) následně udělat časový rozdíl těchto aktivit (Python)
-                     4) a porovnat s průměrnou délkou iterace
-
-
-
-
 */
 
 /* Init project id */
 set @projectId = ?;
-/* Number of iterations for project */
-set @numberOfIterations = (SELECT count(id) FROM `iteration` WHERE superProjectId = @projectId);
-/* Average iteration length */
-set @averageIterationLength = (SELECT AVG(abs(datediff(iteration.endDate, iteration.startDate))) FROM `iteration` WHERE superProjectId = @projectId);
-/* Look for issues that should indicate customer demo or customer feedback */
-select workunitview.duedate as `dueDate`, @numberOfIterations as `numberOfIterations`, @averageIterationLength as `averageIterationLength` FROM `workunitview` where workunitview.projectId = @projectId and (workunitview.name like '%demo%' or workunitview.description like '%demo%' or workunitview.name like '%zákazník%' or workunitview.description like '%zákazník%' or workunitview.name like '%cust%' or workunitview.description like '%cust%' or workunitview.name like '%zadavatel%' or workunitview.description like '%zadavatel%' or workunitview.name like '%předvedení%' or workunitview.description like '%předvedení%' or workunitview.name like '%presentation%' or workunitview.description like '%presentation%') group by workunitview.duedate;
-
-
+/* Number of iterations for given project */
+select COUNT(id) as 'numberOfIterations' from iteration where superProjectId = @projectId;
+/* Select all iteration with detected retrospective activities */
+select  iterationName as 'iterationName', count(name) as 'numberOfIssues' from workunitview where projectId = @projectId and (name like "%schůz%zákazník%" OR name like "%předvedení%zákazník%" OR name LIKE "%zákazn%demo%" OR name like "%schůz%zadavat%" OR name like "%inform%schůz%") group by iterationName;
+/* Look up for wiki pages */
 
