2fe5ab7ad88849ec9fbdffd4bb853e2697184093
Ondřej Váně <onry.vane@gmail.com>
2021-04-15 11:10:22 +0200

    Removed unused utils

--- src/main/java/cz/zcu/fav/kiv/antipatterndetectionapp/utils/Utils.java
+++ src/main/java/cz/zcu/fav/kiv/antipatterndetectionapp/utils/Utils.java
@@ -23,55 +23,6 @@ public class Utils {
 
     private final Logger LOGGER = LoggerFactory.getLogger(Utils.class);
 
-    @Autowired
-    ServletContext context;
-
-    public void getJsonResource() {
-
-        try {
-            URL test = context.getResource(
-                    "/queries/too_long_sprint.sql");
-            LOGGER.error("URL: " + test.toString());
-            BufferedReader read = new BufferedReader(
-                    new InputStreamReader(test.openStream()));
-            String i;
-            while ((i = read.readLine()) != null) {
-                LOGGER.error("SQL: " + i);
-            }
-            read.close();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static String bindValues(String query, List<String> parameters) {
-        for (String parameter : parameters) {
-            query = query.replace("?", parameter);
-        }
-        return query;
-    }
-
-    public static List<String> loadQueryFromFile(String fileName) {
-        List<String> queries = new ArrayList<>();
-        InputStream is = Utils.class.getClassLoader().getResourceAsStream("/queries/" + fileName);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        try {
-            while (reader.ready()) {
-
-                String line = reader.readLine();
-                if (line.startsWith("select") || line.startsWith("set")) {
-                    queries.add(line);
-                }
-
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return queries;
-    }
-
     public static Long[] arrayOfStringsToArrayOfLongs(String[] strings) {
         Long[] longs = new Long[strings.length];
         for (int i = 0; i < strings.length; i++) {
