246e2a400fdfe8a74239970f657ab030316f0c37
Ondřej Váně <onry.vane@gmail.com>
2021-07-20 12:23:09 +0200

    #4 Allow reparameterization in AP detail
    - added form to the AP detail page for editing treshold values
    - added method to the controller for saving new values

--- src/main/java/cz/zcu/fav/kiv/antipatterndetectionapp/controller/AppController.java
+++ src/main/java/cz/zcu/fav/kiv/antipatterndetectionapp/controller/AppController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -104,4 +105,24 @@ public class AppController {
 
         return "configuration";
     }
+
+    @PostMapping("/anti-patterns/{id}")
+    public String antiPatternsPost(Model model,
+                                   @PathVariable Long id,
+                                   @RequestParam(value = "configValues", required = false) String[] configValues,
+                                   @RequestParam(value = "configNames", required = false) String[] configNames,
+                                   RedirectAttributes redirectAttrs) {
+
+        if (antiPatternService.saveNewConfiguration(configNames, configValues)) {
+            redirectAttrs.addFlashAttribute("successMessage", "All threshold values has been successfully saved.");
+        } else {
+            redirectAttrs.addFlashAttribute("errorMessage", "One or more configuration values are not in correct format");
+        }
+
+        model.addAttribute("antiPatterns", antiPatternService.antiPatternToModel(antiPatternService.getAntiPatternById(id)));
+
+        return "redirect:/anti-patterns/{id}";
+    }
+
+
 }
