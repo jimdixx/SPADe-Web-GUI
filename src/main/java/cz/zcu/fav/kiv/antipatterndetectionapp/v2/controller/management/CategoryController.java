7ba346c100c8987d080c6ec331f1b1baad83d6af
stepanekp <1stepanekpetr@gmail.com>
2023-11-18 16:20:15 +0100

    https://kivprogrammers.atlassian.net/browse/TSP2-60 Vykreslení dat z API

--- src/main/java/cz/zcu/fav/kiv/antipatterndetectionapp/v2/controller/management/CategoryController.java
+++ src/main/java/cz/zcu/fav/kiv/antipatterndetectionapp/v2/controller/management/CategoryController.java
@@ -1,24 +1,16 @@
 package cz.zcu.fav.kiv.antipatterndetectionapp.v2.controller.management;
 
-import com.google.gson.Gson;
-import cz.zcu.fav.kiv.antipatterndetectionapp.model.Project;
-import cz.zcu.fav.kiv.antipatterndetectionapp.model.management.Category;
-import cz.zcu.fav.kiv.antipatterndetectionapp.model.management.ProjectInstance;
-import cz.zcu.fav.kiv.antipatterndetectionapp.service.ProjectService;
 import cz.zcu.fav.kiv.antipatterndetectionapp.v2.model.CategoryDto;
 import cz.zcu.fav.kiv.antipatterndetectionapp.v2.model.GeneralResponseDto;
 import cz.zcu.fav.kiv.antipatterndetectionapp.v2.service.management.category.ICategories;
-import cz.zcu.fav.kiv.antipatterndetectionapp.v2.utils.converters.CategoryToDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import java.util.Map;
 
 /**
  * This class contains all endpoints connected to Categories
@@ -30,7 +22,7 @@ public class CategoryController {
 
     private final ICategories categories;
 
-    @GetMapping("/getCategories")
+    @PostMapping("/getCategories")
     public ResponseEntity<GeneralResponseDto<CategoryDto>> getCategories(@RequestBody Map<String, String> requestData) {
         return categories.getResponse(requestData.get("projectId"));
     }
