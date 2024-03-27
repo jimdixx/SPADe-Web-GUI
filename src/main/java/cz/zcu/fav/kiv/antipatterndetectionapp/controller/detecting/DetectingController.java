f9efb921e63e1f6da2177a7578c6806bc53e971b
stepanekp <1stepanekpetr@gmail.com>
2024-03-27 21:11:22 +0100

    Detecting controller edited

--- src/main/java/cz/zcu/fav/kiv/antipatterndetectionapp/controller/detecting/DetectingController.java
+++ src/main/java/cz/zcu/fav/kiv/antipatterndetectionapp/controller/detecting/DetectingController.java
@@ -3,11 +3,11 @@ package cz.zcu.fav.kiv.antipatterndetectionapp.controller.detecting;
 import cz.zcu.fav.kiv.antipatterndetectionapp.service.detecting.DetectingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.HandlerMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -31,6 +31,8 @@ public class DetectingController {
             ResponseEntity<String> response = detectingService.exchangeWithExternalService(url, requestBody, method);
 
             return response;
+        } catch (HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsString());
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
